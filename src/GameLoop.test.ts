672d518dab8fb692c6008e4f6ff05daaa0cb14ac
Patrick Canfield <578371+patreeceeo@users.noreply.github.com>
2021-03-31 15:26:53 -0700

    feat(gameloop): add pauseOnWindowBlur option

--- src/GameLoop.test.ts
+++ src/GameLoop.test.ts
@@ -1,4 +1,19 @@
 import { GameLoop } from "./GameLoop";
+import { _window } from "./globals";
+
+jest.mock("./globals", () => {
+  return {
+    _window: {
+      addEventListener(type: string, listener: any) {
+        if (type === "blur") {
+          (this as any).capturedBlurListener = listener;
+        } else if (type === "focus") {
+          (this as any).capturedFocusListener = listener;
+        }
+      },
+    },
+  };
+});
 
 jest.useFakeTimers();
 describe("GameLoop", () => {
@@ -21,6 +36,26 @@ describe("GameLoop", () => {
     jest.advanceTimersByTime((1000 / 20) * 2);
 
     expect(execute).not.toHaveBeenCalled();
+  });
+
+  test("opt: pauseOnWindowBlur", () => {
+    const execute = jest.fn();
+
+    const sut = new GameLoop(execute, 20, { pauseOnWindowBlur: true });
+
+    sut.start();
+
+    (_window as any).capturedBlurListener();
+
+    jest.advanceTimersByTime((1000 / 20) * 3);
+
+    expect(execute).not.toHaveBeenCalled();
+
+    (_window as any).capturedFocusListener();
+
+    jest.advanceTimersByTime((1000 / 20) * 3);
+
+    expect(execute).toHaveBeenCalledTimes(3);
   });
 
   test.todo("adjust");
--- src/GameLoop.ts
+++ src/GameLoop.ts
@@ -1,18 +1,42 @@
+import { _window } from "./globals";
+
 interface IExecuteFn {
   (deltaMs: number, timeMs: number): void;
 }
 
+interface IOptions {
+  pauseOnWindowBlur?: boolean;
+}
+
 export class GameLoop {
+  _started = false;
   _executeFn: IExecuteFn;
   _frequencyHz: number;
+  _options: IOptions;
   _intervalId: any;
 
-  constructor(executeFn: IExecuteFn, frequencyHz: number) {
+  _foo: any = null;
+
+  constructor(
+    executeFn: IExecuteFn,
+    frequencyHz: number,
+    options: IOptions = {}
+  ) {
     this._executeFn = executeFn;
     this._frequencyHz = frequencyHz;
+    this._options = options;
   }
 
   start() {
+    this._justStart();
+
+    if (this._options.pauseOnWindowBlur) {
+      _window.addEventListener("blur", this.stop.bind(this));
+      _window.addEventListener("focus", this._justStart.bind(this));
+    }
+  }
+
+  _justStart() {
     const intervalMs = 1000 / this._frequencyHz;
     let timeMs = 0;
     this._intervalId = setInterval(() => {
