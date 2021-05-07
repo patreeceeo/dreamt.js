c2df8a6fdffd84384ceb5e9f527006204708fa43
Patrick Canfield <578371+patreeceeo@users.noreply.github.com>
2021-05-07 10:17:32 -0700

    fix(camera): fix prev feat

--- src/camera/index.test.ts
+++ src/camera/index.test.ts
@@ -27,7 +27,7 @@ describe("apply3rdPersonView", () => {
     const target = new Camera();
     const lookDirection = new Euler(0, Math.PI/2, 0);
     const position = new Vector3(0, 0, 0);
-    const expectedRotation = new Euler(-0, -Math.PI/2, 0);
+    const expectedRotation = new Euler(0, -Math.PI/2, 0);
     apply3rdPersonView(target, position, lookDirection, 5, 3);
     expect(target.rotation.toArray()).toEqual(expectedRotation.toArray());
   });
@@ -67,6 +67,6 @@ describe("apply3rdPersonView", () => {
     const lookDirection = new Euler(xRot, 0, 0);
     const position = new Vector3(0, 0, 0);
     apply3rdPersonView(target, position, lookDirection, setback, elevation, bodyCylinderRadius);
-    expect(target.rotation.x).toBe(-lookDirection.x)
+    expect(target.rotation.x).toBeCloseTo(-lookDirection.x)
   });
 });
--- src/camera/index.ts
+++ src/camera/index.ts
@@ -1,10 +1,11 @@
-import { Camera, Euler, Vector3 } from "three";
+import { Camera, Euler, Object3D, Vector3 } from "three";
 import { intersectLineWithPlane } from "../math";
 
 const v1 = new Vector3();
 const v2 = new Vector3();
 const groundNormal = new Vector3(0, -1, 0);
 const e1 = new Euler();
+const o1 = new Object3D();
 
 export function apply3rdPersonView(
   target: Camera,
@@ -45,7 +46,14 @@ export function apply3rdPersonView(
       : fullSetback
     : fullSetback;
 
+  const unrestrictedCamera = o1;
+  const fullSetbackUnrestrictedDelta = v1
+    .set(0, 0, cameraSetback)
+    .applyEuler(lookDirection);
+
+  unrestrictedCamera.position.copy(position).add(fullSetbackUnrestrictedDelta)
+  unrestrictedCamera.lookAt(position);
+
   target.position.copy(targetPosition!);
-  target.lookAt(position);
-  target.rotation.x = - lookDirection.x;
+  target.rotation.copy(unrestrictedCamera.rotation);
 }
