ab55969c94afa18cc0d67fe863d882a97a4b6d65
Patrick Canfield <578371+patreeceeo@users.noreply.github.com>
2021-05-06 17:22:06 -0700

    feat: intersectLineWithPlane function

--- src/math/index.ts
+++ src/math/index.ts
@@ -1,23 +1,59 @@
-import {Euler, Quaternion, Vector2, Vector3, Vector4} from "three";
+import {
+  Euler,
+  Plane,
+  Quaternion,
+  Vector2,
+  Vector3,
+  Vector4,
+  Line3,
+} from "three";
 
 const q1 = new Quaternion();
 const v1 = new Vector3();
-const vUp = new Vector3(0, 1, 0);
 const e1 = new Euler();
+const p1 = new Plane();
+const l1 = new Line3();
 
-export function vectorRoundTo(v: Vector2 | Vector3 | Vector4, decimalPlace = 0) {
+export const vUp = Object.freeze(new Vector3(0, 1, 0));
+
+export function vectorRoundTo(
+  v: Vector2 | Vector3 | Vector4,
+  decimalPlace = 0
+) {
   const factor = Math.pow(10, decimalPlace);
   v.multiplyScalar(factor).round().divideScalar(factor);
 }
 
-export function measureEulerBetweenVectors(target: Euler, vecA: Vector3, vecB: Vector3, qRotIntermediate = q1) {
+export function measureEulerBetweenVectors(
+  target: Euler,
+  vecA: Vector3,
+  vecB: Vector3,
+  qRotIntermediate = q1
+) {
   qRotIntermediate.setFromUnitVectors(vecA, vecB);
   target.setFromQuaternion(qRotIntermediate);
 }
 
-export function calculateEulerBetweenPoints(pointA: Vector3, pointB: Vector3, target = e1, v0Angle = vUp) {
+export function calculateEulerBetweenPoints(
+  pointA: Vector3,
+  pointB: Vector3,
+  target = e1,
+  v0Angle = vUp
+) {
   v1.copy(pointB).sub(pointA).normalize();
-  measureEulerBetweenVectors(target, v0Angle, v1)
+  measureEulerBetweenVectors(target, v0Angle, v1);
   return target;
+}
+
+export function intersectLineWithPlane(
+  linePointA: Vector3,
+  linePointB: Vector3,
+  planeNormal: Vector3,
+  planeConstant: number,
+  target = v1
+): Vector3 | null {
+  l1.set(linePointA, linePointB);
+  p1.set(planeNormal, planeConstant);
+  return p1.intersectLine(l1, target);
 }
 
--- src/math/math.test.ts
+++ src/math/math.test.ts
@@ -1,5 +1,5 @@
-import {Euler, Vector3} from 'three';
-import {calculateEulerBetweenPoints} from './index';
+import { Euler, Vector3 } from "three";
+import { calculateEulerBetweenPoints, intersectLineWithPlane } from "./index";
 
 const PI = Math.PI;
 
@@ -12,33 +12,80 @@ test("calculateEulerBetweenPoints", () => {
 
   expect(result.x).toBeCloseTo(0);
   expect(result.y).toBeCloseTo(0);
-  expect(result.z).toBeCloseTo(PI/4);
+  expect(result.z).toBeCloseTo(PI / 4);
 
-  pointA.set(0, 1, 0)
-  pointB.set(0, 0, 1)
-
+  pointA.set(0, 1, 0);
+  pointB.set(0, 0, 1);
 
   result = calculateEulerBetweenPoints(pointA, pointB);
 
-  expect(result.x).toBeCloseTo(3*PI/4);
+  expect(result.x).toBeCloseTo((3 * PI) / 4);
   expect(result.y).toBeCloseTo(0);
   expect(result.z).toBeCloseTo(0);
 
-  pointA.set(1, 0, 0)
-  pointB.set(0, 0, 0)
+  pointA.set(1, 0, 0);
+  pointB.set(0, 0, 0);
 
   result = calculateEulerBetweenPoints(pointA, pointB);
 
   expect(result.x).toBeCloseTo(0);
   expect(result.y).toBeCloseTo(0);
-  expect(result.z).toBeCloseTo(PI/2);
+  expect(result.z).toBeCloseTo(PI / 2);
 
-  pointA.set(0, 0, 0)
-  pointB.set(0, 1, 0)
+  pointA.set(0, 0, 0);
+  pointB.set(0, 1, 0);
 
   result = calculateEulerBetweenPoints(pointA, pointB);
 
   expect(result.x).toBeCloseTo(0);
   expect(result.y).toBeCloseTo(0);
   expect(result.z).toBeCloseTo(0);
+});
+
+test("intersectLineWithPlane", () => {
+  let result: Vector3 | null;
+
+  result = intersectLineWithPlane(
+    new Vector3(0, 0, 0),
+    new Vector3(2, 0, 0),
+    new Vector3(-1, 0, 0),
+    0.5
+  );
+
+  expect(result!.x).toBe(0.5);
+  expect(result!.y).toBe(0);
+  expect(result!.z).toBe(0);
+
+  result = intersectLineWithPlane(
+    new Vector3(0, 0, 0),
+    new Vector3(0, 2, 0),
+    new Vector3(0, -1, 0),
+    0.5
+  );
+
+  expect(result!.x).toBe(0);
+  expect(result!.y).toBe(0.5);
+  expect(result!.z).toBe(0);
+
+  result = intersectLineWithPlane(
+    new Vector3(0, 0, 0),
+    new Vector3(0, -2, 0),
+    new Vector3(0, -1, 0),
+    -0.5
+  );
+
+  expect(result!.x).toBe(0);
+  expect(result!.y).toBe(-0.5);
+  expect(result!.z).toBe(0);
+
+  result = intersectLineWithPlane(
+    new Vector3(2, 0, 0),
+    new Vector3(0, 2, 0),
+    new Vector3(0, -1, 0),
+    0.5
+  );
+
+  expect(result!.x).toBe(1.5);
+  expect(result!.y).toBe(0.5);
+  expect(result!.z).toBe(0);
 });
