f6678fd789fc32116479a55e7a08b36999b09381
Patrick Canfield <578371+patreeceeo@users.noreply.github.com>
2021-05-06 17:24:38 -0700

    feat: specify elevation of 3rd person camera

--- src/camera/index.ts
+++ src/camera/index.ts
@@ -1,9 +1,29 @@
-import {Camera, Euler, Vector3} from "three";
+import { Camera, Euler, Vector3 } from "three";
+import { intersectLineWithPlane } from "../math";
 
 const v1 = new Vector3();
+const v2 = new Vector3();
+const groundNormal = new Vector3(0, -1, 0);
 
-export function apply3rdPersonView(target: Camera, position: Vector3, lookDirection: Euler, amount: number) {
-  v1.set(0, 0, amount).applyEuler(lookDirection);
-  target.position.copy(position).sub(v1)
+export function apply3rdPersonView(
+  target: Camera,
+  position: Vector3,
+  lookDirection: Euler,
+  cameraSetback: number,
+  cameraElevation: number
+) {
+  const fullSetbackDelta = v1
+    .set(0, 0, cameraSetback)
+    .applyEuler(lookDirection);
+  const fullSetback = v2.subVectors(target.position, fullSetbackDelta);
+
+  const targetPosition = lookDirection.x < 0 ? intersectLineWithPlane(
+    target.position,
+    fullSetback,
+    groundNormal,
+    target.position.y - cameraElevation
+  ) :  fullSetback;
+
+  target.position.copy(targetPosition!);
   target.lookAt(position);
 }
